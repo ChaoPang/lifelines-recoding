31a92f7ae9401c403c07d2bb0c2cc5c24ba93170
Chao Pang <ChaoPang229@gmail.com>
2014-12-01 17:17:25 +0100

    auto formated by eclipse

--- src/main/resources/js/common-function.js
+++ src/main/resources/js/common-function.js
@@ -1,200 +1,328 @@
 (function($, molgenis) {
-	
-	molgenis.createTable = function(options){
-		
+
+	molgenis.createTable = function(options) {
+
 		var queryString = options.queryString;
 		var hits = options.hits;
 		var parentElement = options.parentElement;
-		var addCodeFunction = (options.addCode && typeof options.addCode === 'function' ? options.addCode : molgenis.defaultAddCode);
-		var unknownCode = (options.unknownCode && typeof options.unknownCode === 'function' ? options.unknownCode : null);
-		
-		if(hits.length > 0){
+		var addCodeFunction = (options.addCode
+				&& typeof options.addCode === 'function' ? options.addCode
+				: molgenis.defaultAddCode);
+		var unknownCode = (options.unknownCode
+				&& typeof options.unknownCode === 'function' ? options.unknownCode
+				: null);
+
+		if (hits.length > 0) {
 			var table = $('<table />').addClass('table table-bordered');
-			$('<tr />').append('<th>Name</th><th>Code</th><th>Code system</th><th>Score</th><th>Frequency</th><th>Select</th>').appendTo(table);
-			$.each(hits, function(index, hit){
+			$('<tr />')
+					.append(
+							'<th>Name</th><th>Code</th><th>Code system</th><th>Score</th><th>Frequency</th><th>Select</th>')
+					.appendTo(table);
+			$.each(hits, function(index, hit) {
 				var columnValueMap = hit.columnValueMap;
-				var row = $('<tr />').append('<td>' + columnValueMap.name + '</td>').
-					append('<td>' + columnValueMap.code + '</td>').
-						append('<td>' + columnValueMap.codesystem + '</td>').
-							append('<td>' + hit.score + '%</td>').append('<td>' + hit.frequency + '</td>').
-								append('<td><input type="checkbox"></td>');
+				var row = $('<tr />').append(
+						'<td>' + columnValueMap.name + '</td>').append(
+						'<td>' + columnValueMap.code + '</td>').append(
+						'<td>' + columnValueMap.codesystem + '</td>').append(
+						'<td>' + hit.score + '%</td>').append(
+						'<td>' + hit.frequency + '</td>').append(
+						'<td><input type="checkbox"></td>');
 				table.append(row);
 				row.data('searchHit', hit);
 			});
-			
-			$.each(table.find('input:checkbox'), function(index, checkbox){
-				//when the checkbox is checked, the other checkbox are disabled
-				$(checkbox).click(function(){
-					if($(checkbox).is(':checked')){
-						table.find('input:checkbox:not(:eq(' + index + '))').attr('checked', false);
-					}
-				});
+
+			$.each(table.find('input:checkbox'), function(index, checkbox) {
+				// when the checkbox is checked, the other checkbox are disabled
+				$(checkbox).click(
+						function() {
+							if ($(checkbox).is(':checked')) {
+								table.find(
+										'input:checkbox:not(:eq(' + index
+												+ '))').attr('checked', false);
+							}
+						});
 			});
-			
-			var tableContainer = $('<div />').addClass('row-fluid').appendTo(parentElement);
-			var controlContainer = $('<div />').addClass('row-fluid').appendTo(parentElement);
+
+			var tableContainer = $('<div />').addClass('row-fluid').appendTo(
+					parentElement);
+			var controlContainer = $('<div />').addClass('row-fluid').appendTo(
+					parentElement);
 			var selectUnknowButton = $('<button class="btn btn-danger" type="button">Unknow code</button>');
 			var selectCodeButton = $('<button class="btn btn-primary btn-float-right" type="button">Code data</button>');
 			var addCodeButton = $('<button class="btn btn-float-right-align" type="button">Code and add</button>');
-			$('<div />').addClass('span12').append(table).appendTo(tableContainer);
-			$('<div />').addClass('span12').append(selectUnknowButton).append(addCodeButton).append(selectCodeButton).appendTo(controlContainer);
-			
-			selectCodeButton.click(function(){
-				$.each(table.find('input:checkbox'), function(index, checkbox){
-					if($(checkbox).is(':checked')){
-						addCodeFunction(queryString, $(checkbox).parents('tr:first').data('searchHit'), false);
-						setTimeout(function(){
+			$('<div />').addClass('span12').append(table).appendTo(
+					tableContainer);
+			$('<div />').addClass('span12').append(selectUnknowButton).append(
+					addCodeButton).append(selectCodeButton).appendTo(
+					controlContainer);
+
+			selectCodeButton.click(function() {
+				$.each(table.find('input:checkbox'), function(index, checkbox) {
+					if ($(checkbox).is(':checked')) {
+						addCodeFunction(queryString, $(checkbox).parents(
+								'tr:first').data('searchHit'), false);
+						setTimeout(function() {
 							location.reload();
-						},1500);
+						}, 1500);
 					}
 				});
 			});
-			
-			addCodeButton.click(function(){
-				$.each(table.find('input:checkbox'), function(index, checkbox){
-					if($(checkbox).is(':checked')){
-						addCodeFunction(queryString, $(checkbox).parents('tr:first').data('searchHit'), true);
-						setTimeout(function(){
+
+			addCodeButton.click(function() {
+				$.each(table.find('input:checkbox'), function(index, checkbox) {
+					if ($(checkbox).is(':checked')) {
+						addCodeFunction(queryString, $(checkbox).parents(
+								'tr:first').data('searchHit'), true);
+						setTimeout(function() {
 							location.reload();
-						},1500);
+						}, 1500);
 					}
 				});
 			});
-			
-			selectUnknowButton.click(function(){
-				if(unknownCode){
+
+			selectUnknowButton.click(function() {
+				if (unknownCode) {
 					unknownCode({
 						'codesystem' : hits[0].columnValueMap.codesystem,
 						'query' : queryString
 					});
-					setTimeout(function(){
+					setTimeout(function() {
 						location.reload();
-					},1500);
-				}else{
+					}, 1500);
+				} else {
 					console.log("The function is not defined!");
 				}
 			});
-			
-		}else{
-			parentElement.append('<div class="row-fluid middle-text"><br><br><center>No codes found for this input <strong>' + queryString + '</strong>. Please change your input string and try again!</center></div>');
+
+		} else {
+			parentElement
+					.append('<div class="row-fluid middle-text"><br><br><center>No codes found for this input <strong>'
+							+ queryString
+							+ '</strong>. Please change your input string and try again!</center></div>');
 		}
 	};
-	
-	molgenis.findAllCodeSystems = function(callback){
+
+	molgenis.findAllCodeSystems = function(callback) {
 		$.ajax({
 			type : 'GET',
-			url :  '/view/codesystems',
+			url : '/view/codesystems',
 			contentType : 'application/json',
-			success : function(data){
-				if(callback) callback(data)
+			success : function(data) {
+				if (callback)
+					callback(data);
 			}
 		});
 	};
-	
-	molgenis.retrieveAllCodes = function (codeSystem, container){
-		if(codeSystem !== null && codeSystem !== ''){
-			$.ajax({
-				type : 'GET',
-				url :  '/view/alldocs?codeSystem=' + codeSystem,
-				contentType : 'application/json',
-				success : function(data){
-					var table = $('<table />').addClass('table table-bordered');
-					table.append('<tr><th>Name</th><th>Code</th><th>Code system</th><th>Original</th><th>Frequency</th><th>date added</th><th>Delete</th></tr>');
-					$.each(data.results, function(index, hit){
-						var columnValueMap = hit.columnValueMap;
-						var row = $('<tr />').appendTo(table); 
-						var deleteButton = $('<span><i class="icon icon-trash"></i></span>').css('cursor', 'pointer');
-						row.append('<td>' + columnValueMap.name + '</td><td>' + columnValueMap.code + 
-								'</td><td>' + columnValueMap.codesystem + '</td><td>' + columnValueMap.original + 
-									'</td><td>' + hit.frequency + '</td><td>' + hit.columnValueMap.date + '</td>');
-						$('<td />').append(deleteButton).appendTo(row);
-						
-						deleteButton.click(function(){
-							if(!hit.columnValueMap.original){
-								molgenis.deleteCode(hit.documentId, hit.columnValueMap.codesystem, function(data){
-									if(data.success) $(row).remove();
-									molgenis.createAlert([data], data.success ? 'success' : 'error', container);
-								});
-							}else{
-								molgenis.createAlert([{'message' : 'The code ' + hit.columnValueMap.code + ' ( <strong>' + hit.columnValueMap.name + '</strong> ) is original and cannot be removed!'}], 'warning', container);
-							}
-						});
+
+	molgenis.downloadCodeSystem = function(codeSystem) {
+		$.ajax({
+			type : 'GET',
+			url : '/view/download/' + codeSystem
+		});
+	};
+
+	molgenis.retrieveAllCodes = function(codeSystem, container) {
+		if (codeSystem !== null && codeSystem !== '') {
+			$
+					.ajax({
+						type : 'GET',
+						url : '/view/alldocs?codeSystem=' + codeSystem,
+						contentType : 'application/json',
+						success : function(data) {
+							var table = $('<table />').addClass(
+									'table table-bordered');
+							table
+									.append('<tr><th>Name</th><th>Code</th><th>Code system</th><th>Original</th><th>Frequency</th><th>date added</th><th>Delete</th></tr>');
+							$
+									.each(
+											data.results,
+											function(index, hit) {
+												var columnValueMap = hit.columnValueMap;
+												var row = $('<tr />').appendTo(
+														table);
+												var deleteButton = $(
+														'<span><i class="icon icon-trash"></i></span>')
+														.css('cursor',
+																'pointer');
+												row
+														.append('<td>'
+																+ columnValueMap.name
+																+ '</td><td>'
+																+ columnValueMap.code
+																+ '</td><td>'
+																+ columnValueMap.codesystem
+																+ '</td><td>'
+																+ columnValueMap.original
+																+ '</td><td>'
+																+ hit.frequency
+																+ '</td><td>'
+																+ hit.columnValueMap.date
+																+ '</td>');
+												$('<td />')
+														.append(deleteButton)
+														.appendTo(row);
+
+												deleteButton
+														.click(function() {
+															if (!hit.columnValueMap.original) {
+																molgenis
+																		.deleteCode(
+																				hit.documentId,
+																				hit.columnValueMap.codesystem,
+																				function(
+																						data) {
+																					if (data.success)
+																						$(
+																								row)
+																								.remove();
+																					molgenis
+																							.createAlert(
+																									[ data ],
+																									data.success ? 'success'
+																											: 'error',
+																									container);
+																				});
+															} else {
+																molgenis
+																		.createAlert(
+																				[ {
+																					'message' : 'The code '
+																							+ hit.columnValueMap.code
+																							+ ' ( <strong>'
+																							+ hit.columnValueMap.name
+																							+ '</strong> ) is original and cannot be removed!'
+																				} ],
+																				'warning',
+																				container);
+															}
+														});
+											});
+							$(container).empty().append(table);
+						}
 					});
-					$(container).empty().append(table);
-				}
-			});
 		}
 	};
-	
-	molgenis.deleteCode = function(documentId, codeSystem, callback){
+
+	molgenis.deleteCode = function(documentId, codeSystem, callback) {
 		$.ajax({
 			type : 'POST',
-			url :  '/view/delete',
-			data : JSON.stringify({'documentId' : documentId, 'codesystem' : codeSystem}),
+			url : '/view/delete',
+			data : JSON.stringify({
+				'documentId' : documentId,
+				'codesystem' : codeSystem
+			}),
 			contentType : 'application/json',
-			success : function(data){
-				if(callback && typeof callback === 'function'){
+			success : function(data) {
+				if (callback && typeof callback === 'function') {
 					callback(data);
 				}
 			}
 		});
 	};
-	
-	molgenis.recoverBackup = function(codingJobName){
+
+	molgenis.recoverBackup = function(codingJobName) {
 		$.ajax({
 			type : 'POST',
-			url :  '/recode/recovery',
-			data : JSON.stringify({'codingJobName' : codingJobName}),
+			url : '/recode/recovery',
+			data : JSON.stringify({
+				'codingJobName' : codingJobName
+			}),
 			contentType : 'application/json',
-			success : function(data){
-				if(data.success){
+			success : function(data) {
+				if (data.success) {
 					window.location.href = '/recode';
-				}else{
+				} else {
 					console.log(data.message);
 				}
 			}
 		});
-	}
-	
-	molgenis.retrieveBackups = function(parentElement){
-		$.ajax({
-			type : 'GET',
-			url :  '/backup/list',
-			contentType : 'application/json',
-			success : function(data){
-				var table = $('<table />').addClass('table').appendTo(parentElement);
-				table.append('<tr><th>Job name</th><th>Code system</th><th>Date</th><th style="width:20%;">Recover</th></tr>');
-				$.each(data.results, function(index, hit){
-					var recoveyButton = $('<button type="button" class="btn">Recover</button>');
-					var confirmButton = $('<button type="button" class="btn btn-primary" style="float:right;">Confirm</button>');
-					var cancelButton = $('<button type="button" class="btn">Cancel</button>');
-					var row = $('<tr />').append('<td>' + hit.columnValueMap.name +'</td>').append('<td>' + hit.columnValueMap.codesystem +'</td>').append('<td>' + new Date(hit.columnValueMap.addedDate) +'</td>').appendTo(table);
-					$('<td />').append(recoveyButton).appendTo(row);
-					if(!data.notAllow){
-						recoveyButton.click(function(){
-							recoveyButton.parents('td:eq(0)').append(confirmButton).append(cancelButton);
-							recoveyButton.hide();
-							molgenis.createAlert([{'message':'The current job will be <strong>removed</strong> if you recover this backup, are you sure?'}],'warning', parentElement);
-							confirmButton.click(function(){
-								molgenis.recoverBackup(hit.columnValueMap.name);
-							});
-							cancelButton.click(function(){
-								recoveyButton.show();
-								confirmButton.remove();
-								cancelButton.remove();
-							})
-						});
+	};
+
+	molgenis.retrieveBackups = function(parentElement) {
+		$
+				.ajax({
+					type : 'GET',
+					url : '/backup/list',
+					contentType : 'application/json',
+					success : function(data) {
+						var table = $('<table />').addClass('table').appendTo(
+								parentElement);
+						table
+								.append('<tr><th>Job name</th><th>Code system</th><th>Date</th><th style="width:20%;">Recover</th></tr>');
+						$
+								.each(
+										data.results,
+										function(index, hit) {
+											var recoveyButton = $('<button type="button" class="btn">Recover</button>');
+											var confirmButton = $('<button type="button" class="btn btn-primary" style="float:right;">Confirm</button>');
+											var cancelButton = $('<button type="button" class="btn">Cancel</button>');
+											var row = $('<tr />')
+													.append(
+															'<td>'
+																	+ hit.columnValueMap.name
+																	+ '</td>')
+													.append(
+															'<td>'
+																	+ hit.columnValueMap.codesystem
+																	+ '</td>')
+													.append(
+															'<td>'
+																	+ new Date(
+																			hit.columnValueMap.addedDate)
+																	+ '</td>')
+													.appendTo(table);
+											$('<td />').append(recoveyButton)
+													.appendTo(row);
+											if (!data.notAllow) {
+												recoveyButton
+														.click(function() {
+															recoveyButton
+																	.parents(
+																			'td:eq(0)')
+																	.append(
+																			confirmButton)
+																	.append(
+																			cancelButton);
+															recoveyButton
+																	.hide();
+															molgenis
+																	.createAlert(
+																			[ {
+																				'message' : 'The current job will be <strong>removed</strong> if you recover this backup, are you sure?'
+																			} ],
+																			'warning',
+																			parentElement);
+															confirmButton
+																	.click(function() {
+																		molgenis
+																				.recoverBackup(hit.columnValueMap.name);
+																	});
+															cancelButton
+																	.click(function() {
+																		recoveyButton
+																				.show();
+																		confirmButton
+																				.remove();
+																		cancelButton
+																				.remove();
+																	});
+														});
+											}
+										});
+						if (data.notAllow) {
+							molgenis
+									.createAlert(
+											[ {
+												'message' : 'The system is either uploading new data or backing up, if you want to recover old jobs, please wait until it`s finished!'
+											} ], 'warning', parentElement);
+							table.find('button').attr('disabled', 'true');
+						}
+						;
 					}
 				});
-				if(data.notAllow){
-					molgenis.createAlert([{'message':'The system is either uploading new data or backing up, if you want to recover old jobs, please wait until it`s finished!'}],'warning', parentElement);
-					table.find('button').attr('disabled', 'true');
-				};	
-			}
-		});
 	};
-	
-	molgenis.defaultAddCode = function(query, hit, callback){
+
+	molgenis.defaultAddCode = function(query, hit, callback) {
 		var request = {
 			'data' : {
 				'code' : hit.columnValueMap.code,
@@ -207,16 +335,17 @@
 		};
 		$.ajax({
 			type : 'POST',
-			url :  '/add',
+			url : '/add',
 			data : JSON.stringify(request),
 			contentType : 'application/json',
-			success : function(data){
-				if(callback && typeof callback === 'function') callback(data);
+			success : function(data) {
+				if (callback && typeof callback === 'function')
+					callback(data);
 			}
 		});
 	};
-	
-	molgenis.findCode = function(query, originalQuery, codeSystem, callback){
+
+	molgenis.findCode = function(query, originalQuery, codeSystem, callback) {
 		var request = {
 			'query' : query,
 			'originalQuery' : originalQuery,
@@ -224,15 +353,16 @@
 		};
 		$.ajax({
 			type : 'POST',
-			url :  '/find',
+			url : '/find',
 			data : JSON.stringify(request),
 			contentType : 'application/json',
-			success : function(data){
-				if(callback) callback(data);
+			success : function(data) {
+				if (callback)
+					callback(data);
 			}
 		});
 	};
-	
+
 	molgenis.createAlert = function(alerts, type, container) {
 		if (type !== 'error' && type !== 'warning' && type !== 'success')
 			type = 'error';
@@ -257,7 +387,7 @@
 
 		container.prepend(items.join(''));
 	};
-	
+
 	molgenis.naturalSort = function(a, b) {
 		var re = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi, sre = /(^[ ]*|[ ]*$)/g, dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/, hre = /^0x[0-9a-f]+$/i, ore = /^0/, i = function(
 				s) {
@@ -282,7 +412,7 @@
 			else if (xD > yD)
 				return 1;
 		// natural sorting through split numeric strings and default strings
-		for ( var cLoc = 0, numS = Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
+		for (var cLoc = 0, numS = Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
 			// find floats not starting with '0', string or 0 if not defined
 			// (Clint Priest)
 			oFxNcL = !(xN[cLoc] || '').match(ore) && parseFloat(xN[cLoc])
