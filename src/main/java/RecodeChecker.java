f68f46997a7e11da5f80e45d4fd658aea399c7fe
Chao Pang <ChaoPang229@gmail.com>
2015-01-14 11:55:48 +0100

    RecodeChecker also checks the missing values for existing participants

--- src/main/java/RecodeChecker.java
+++ src/main/java/RecodeChecker.java
@@ -2,17 +2,24 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.molgenis.data.Entity;
 import org.molgenis.data.csv.CsvRepository;
 import org.molgenis.data.csv.CsvWriter;
 import org.molgenis.data.processor.CellProcessor;
+import org.molgenis.data.support.MapEntity;
+
+import com.google.common.collect.Iterables;
 
 public class RecodeChecker
 {
+	private final static List<String> COLUMN_HEADERS = Arrays.asList("Name_1", "Name_2", "Name_3", "Name_4");
+
 	public RecodeChecker(String[] args) throws IOException, InvalidFormatException
 	{
 		File file = new File(args[0]);
@@ -25,22 +32,47 @@ public class RecodeChecker
 		CsvWriter csvWriter = new CsvWriter(outputFile, ';');
 		csvWriter.writeAttributes(inputDataRepository.getEntityMetaData().getAtomicAttributes());
 
-		Set<String> identifiers = new HashSet<String>();
-
+		Map<String, Map<Integer, String>> downloadDataSet = new HashMap<String, Map<Integer, String>>();
 		for (Entity entity : downloadDataRepository)
 		{
-			identifiers.add(entity.getString("Identifier"));
+			String identifier = entity.getString("Identifier");
+			downloadDataSet.put(identifier, new HashMap<Integer, String>());
+			for (String columnHeader : COLUMN_HEADERS)
+			{
+				if (!StringUtils.isEmpty(entity.getString(columnHeader)))
+				{
+					downloadDataSet.get(identifier).put(COLUMN_HEADERS.indexOf(columnHeader),
+							entity.getString(columnHeader));
+				}
+			}
 		}
 
 		int count = 0;
-
 		for (Entity entity : inputDataRepository)
 		{
-			if (!identifiers.contains(entity.getString("Identifier")))
+			String identifier = entity.getString("Identifier");
+			if (!downloadDataSet.containsKey(identifier))
 			{
 				count++;
 				System.out.println(entity);
-				csvWriter.add(entity);
+				// csvWriter.add(entity);
+			}
+			else
+			{
+				Entity outputEntity = new MapEntity();
+				for (String columnHeader : COLUMN_HEADERS)
+				{
+					if (!StringUtils.isEmpty(entity.getString(columnHeader)))
+					{
+						if (!downloadDataSet.get(identifier).containsKey(COLUMN_HEADERS.indexOf(columnHeader)))
+						{
+							if (StringUtils.isEmpty(outputEntity.getString("Identifier"))) outputEntity.set(
+									"Identifier", identifier);
+							outputEntity.set(columnHeader, entity.getString(columnHeader));
+						}
+					}
+				}
+				if (Iterables.size(outputEntity.getAttributeNames()) != 0) csvWriter.add(outputEntity);
 			}
 		}
 
